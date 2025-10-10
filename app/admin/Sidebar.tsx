6feb85f3ecfd8c4fa0cf2e89782b0dfb4f0e7a9c
Arowolo Tunmise <tunmisearowolo094@gmail.com>
2025-10-10 15:16:43 +0100

    add key

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
       {adminLinks.map(link => {
         const isActivePage = pathname === link.href
         const variant = isActivePage ? 'default': 'ghost'
-        return <Button asChild className="w-full mb-2 capitalize font-normal justify-start" variant={variant}>
+        return <Button key={link.href} asChild className="w-full mb-2 capitalize font-normal justify-start" variant={variant}>
           <Link key={link.href} href={link.href}> {link.label} </Link>
         </Button> 
       })}
